f7980c1f0cc87b22d6bedafe0931dadfd0c8affb
Nathaniel Martin <59941301+UpGoerFive@users.noreply.github.com>
2022-02-25 14:19:27 -0600

    Basic model run, not validated

--- data_generation.py
+++ data_generation.py
@@ -1,7 +1,7 @@
-from turtle import position
 import numpy as np
 import pandas as pd
 import chess
+
 
 def position_generator(position_df, batch_size=32):
     """
@@ -9,13 +9,15 @@ def position_generator(position_df, batch_size=32):
     """
     idx = 0
     while True:
-        targets = position_df.iloc[idx:idx + batch_size -1, 1].to_numpy()
-        positions = position_df.iloc[idx:idx + batch_size -1, 0].apply(fix_positions)
-        yield (positions, targets)
+        targets = position_df.iloc[idx:idx + batch_size, 1].to_numpy()
+        positions = np.stack(position_df.iloc[idx:idx + batch_size, 0].apply(fix_positions))
         idx += batch_size
+        yield (positions, targets)
 
 def fix_positions(position_string):
     """
     Deals with weird newline thing until I can figure out why things were saved that way.
     """
     return np.fromstring(position_string.replace('\n', '')[1: -1], dtype=np.float, sep=' ').reshape((8,8,13))
+
+# class PosGen:
