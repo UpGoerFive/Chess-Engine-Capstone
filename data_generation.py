c3ae203800f6864aa381771d86a776f0d7c0f48a
Nathaniel Martin <59941301+UpGoerFive@users.noreply.github.com>
2022-02-25 11:13:09 -0600

    data generator for training

---
+++ data_generation.py
@@ -0,0 +1,21 @@
+from turtle import position
+import numpy as np
+import pandas as pd
+import chess
+
+def position_generator(position_df, batch_size=32):
+    """
+    Takes a data frame and yields a tuple of numpy arrays of batch size.
+    """
+    idx = 0
+    while True:
+        targets = position_df.iloc[idx:idx + batch_size -1, 1].to_numpy()
+        positions = position_df.iloc[idx:idx + batch_size -1, 0].apply(fix_positions, axis=1)
+        yield (positions, targets)
+        idx += batch_size
+
+def fix_positions(position_series):
+    """
+    Deals with weird newline thing until I can figure out why things were saved that way.
+    """
+    return np.fromstring(position_series.Position.replace('\n', '')[1: -1], dtype=np.float, sep=' ')
