7fb7386211b4d45db263e61a43b6804db442f2f2
Nathaniel Martin <59941301+UpGoerFive@users.noreply.github.com>
2022-02-25 17:11:36 -0600

    started better generator

--- data_generation.py
+++ data_generation.py
@@ -20,4 +20,15 @@ def fix_positions(position_string):
     """
     return np.fromstring(position_string.replace('\n', '')[1: -1], dtype=np.float, sep=' ').reshape((8,8,13))
 
-# class PosGen:
+class PosGen:
+    def __init__(self, position_df, batch_size=32):
+        self.batch_size = batch_size
+        self.df = position_df
+        self.x = position_df.iloc[:, 0]
+        self.y = position_df.iloc[:, 1]
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, idx):
+        pass
