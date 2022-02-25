4db21f9febfb375b0d1d76fbb09d970cabcd731e
Nathaniel Martin <59941301+UpGoerFive@users.noreply.github.com>
2022-02-25 11:51:37 -0600

    fixed generator

--- data_generation.py
+++ data_generation.py
@@ -10,12 +10,12 @@ def position_generator(position_df, batch_size=32):
     idx = 0
     while True:
         targets = position_df.iloc[idx:idx + batch_size -1, 1].to_numpy()
-        positions = position_df.iloc[idx:idx + batch_size -1, 0].apply(fix_positions, axis=1)
+        positions = position_df.iloc[idx:idx + batch_size -1, 0].apply(fix_positions)
         yield (positions, targets)
         idx += batch_size
 
-def fix_positions(position_series):
+def fix_positions(position_string):
     """
     Deals with weird newline thing until I can figure out why things were saved that way.
     """
-    return np.fromstring(position_series.Position.replace('\n', '')[1: -1], dtype=np.float, sep=' ')
+    return np.fromstring(position_string.replace('\n', '')[1: -1], dtype=np.float, sep=' ').reshape((8,8,13))
