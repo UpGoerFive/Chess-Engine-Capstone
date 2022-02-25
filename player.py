8976ec5d169d5f6048a994cfd03ccf3a742f8311
Nathaniel Martin <59941301+UpGoerFive@users.noreply.github.com>
2022-02-25 14:47:44 -0600

    model can predict, badly

--- player.py
+++ player.py
@@ -6,7 +6,7 @@ from tensorflow.keras import models
 
 class Player:
     def __init__(self, model, start_fen=None):
-        self.model = model
+        self.model = models.load_model(model)
         self.board = chess.Board(fen=start_fen)
 
     def play_move(self):
@@ -19,7 +19,8 @@ class Player:
             move_vals.append(move, quality)
         best_move = max(move_vals, key=lambda pair: pair[1])
         self.board.push_san(best_move[0])
-        return best_move[0]
+        return best_move[0], self.board
 
     def op_move(self, move):
         self.board.push_uci(move)
+        return self.board
