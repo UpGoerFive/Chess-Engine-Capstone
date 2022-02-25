6916dc9ef3a37f354be554a355e11bfca7815530
Nathaniel Martin <59941301+UpGoerFive@users.noreply.github.com>
2022-02-25 16:06:35 -0600

    Barely working

--- player.py
+++ player.py
@@ -3,24 +3,46 @@ import pandas as pd
 import chess
 import tensorflow as tf
 from tensorflow.keras import models
+from fenpreprocessing import fen_to_array
 
 class Player:
-    def __init__(self, model, start_fen=None):
+    def __init__(self, model, start_fen=chess.STARTING_FEN):
+        """
+        Basic player that uses python chess to make moves.
+        """
         self.model = models.load_model(model)
         self.board = chess.Board(fen=start_fen)
+        self.show_board()
+
+    def show_board(self):
+        """
+        Displays board for play.
+        """
+        return self.board
 
     def play_move(self):
+        """
+        Plays best predicted move and displays board.
+        """
         options = list(self.board.legal_moves)
-        move_vals = []
+        fens = []
+
         for move in options:
-            self.board.push_san(move)
-            quality = self.model.predict(self.board.fen())
+            self.board.push(move)
+            fens.append(self.board.fen())
             self.board.pop()
-            move_vals.append(move, quality)
-        best_move = max(move_vals, key=lambda pair: pair[1])
-        self.board.push_san(best_move[0])
-        return best_move[0], self.board
+
+        fens = np.array([fen_to_array(fen).reshape(8,8,13) for fen in fens]) # This is slow, but I just need something working first
+        move_vals = self.model.predict(fens)
+        best_move = options[np.argmax(move_vals)]
+
+        self.board.push(best_move)
+        self.show_board()
+        return best_move
 
     def op_move(self, move):
-        self.board.push_uci(move)
-        return self.board
+        """
+        Receives opponent move.
+        """
+        self.board.push_san(move)
+        self.show_board()
