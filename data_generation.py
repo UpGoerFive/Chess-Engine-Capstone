16ebf9a672bea950bc74d9e95e33fdb2e4326818
Nathaniel Martin <59941301+UpGoerFive@users.noreply.github.com>
2022-03-02 21:20:26 -0600

    Update generator to use full dataset

--- data_generation.py
+++ data_generation.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pandas as pd
 import chess
+import random
 import math
 from tensorflow.keras.utils import Sequence
+from fenpreprocessing import fen_to_array
 
 
 def position_generator(position_df, batch_size=32):
@@ -22,16 +24,18 @@ def fix_positions(position_string):
     """
     return np.fromstring(position_string.replace('\n', '')[1: -1], dtype=np.float, sep=' ').reshape((8,8,13))
 
-class PosGen(Sequence):
+class ChessPositionGen(Sequence):
     """
-    Class based generator based on the Keras documentation for the Sequence class.
+    Data generator class based on the Keras documentation for the Sequence class.
     """
-    def __init__(self, position_df, xlabel, ylabel, batch_size=32):
+    def __init__(self, position_df, shuffle=True, batch_size=32, xpos=0, ypos=1):
         self.batch_size = batch_size
         self.df = position_df
         self.df_index = position_df.index
-        self.x = xlabel
-        self.y = ylabel
+        self.shuffle = shuffle
+        self.xpos = xpos
+        self.ypos = ypos
+        self.on_epoch_end()
 
     def __len__(self):
         """
@@ -41,8 +45,51 @@ class PosGen(Sequence):
 
     def __getitem__(self, idx):
         """
-        Adapted from Keras documentation.
+        Adapted from Keras documentation, and helpful blog here: https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
         """
-        batch = self.df_index[idx * self.batch_size:(idx + 1) * self.batch_size]
 
-        return np.array([fix_positions(position_array) for position_array in self.df.loc[batch, self.x]]), np.array(self.df.loc[batch, self.y])
+        # Pulls out shuffled indices from df_index using indices from on_epoch_end
+        batch_indices = self.indices[idx * self.batch_size:(idx + 1) * self.batch_size]
+        batch = self.df_index[batch_indices]
+
+        return self.__data_generation(batch)
+
+    def __data_generation(self, indices, sample_size=4):
+        """
+        Returns properly converted data for a batch. Adapts code from possible moves function in fenpreprocessing.py.
+        Each starting FEN is converted to 1 correct move array, and sample_size randomly selected incorrect moves.
+        """
+        array_list = []
+        target_list = []
+
+        for fen_entry in self.df.loc[indices, :].values:
+
+            # Initialize board and target move
+            start_fen = fen_entry[self.xpos]
+            target = chess.Move.from_uci(fen_entry[self.ypos])
+            board = chess.Board(start_fen)
+
+            # Create sampled move list. Random Sampling so that data is less imbalanced.
+            move_list = list(board.legal_moves)
+            move_list = random.sample(move_list, min(sample_size, len(move_list)))
+            if target not in move_list:
+                move_list.append(target)
+
+            # Create board array and label for each position in move list
+            for move in move_list:
+                board.push(move)
+                candidate_fen = board.fen()
+                target_val = 1. if move == target else 0.
+                array_list.append(fen_to_array(candidate_fen).reshape((8,8,13)))
+                target_list.append(target_val)
+                board.pop()
+        return np.array(array_list), np.array(target_list)
+
+
+    def on_epoch_end(self):
+        """
+        Shuffles indices after each epoch.
+        """
+        self.indices = np.arange(len(self.df_index))
+        if self.shuffle == True:
+            np.random.shuffle(self.indices)
